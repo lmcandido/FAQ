2b28462ad6065790516a9c754e98eb7ac2aff9df
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 13:06:10 -0700

    script change

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleAnswer(element) {
     element.classList.toggle("active");
     const answer = element.nextElementSibling;
+    const image = element.querySelector('.clickable-image')
 
     const image1 = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
     const image2 = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-minus.svg";
 
     if (answer.style.display === "block") {
         answer.style.display = "none";
-        element.querySelector('.clickable-image').imageElement.scr = image1;
+        image.imageElement.scr = image1;
     } else {
         answer.style.display = "block";
-        element.querySelector('.clickable-image').imageElement.scr = image2;
+        image.imageElement.scr = image2;
     }
 }
