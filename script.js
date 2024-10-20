7c5716efc189c89d0aef342c5b71a655a3d300b1
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 14:43:29 -0700

    changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function toggleAnswer(element) {
     element.classList.toggle("active");
-    const answer = element.NextElementSibling;
-    //const imageElements = document.querySelector(".clickable-image");
+    const answer = element.nextElementSibling;
+    const imageElements = document.querySelector(".clickable-image");
 
     if (answer.style.display === "block") {
         answer.style.display = "none";
-        //imageElements.src = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
+        imageElements.src = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
     } else {
         answer.style.display = "block";
-        //imageElements.src = "https://raw.githubusercontent.com/lmcandido/FAQ/0ff113a00d8f341ef2144542f39c7e33b9aa6f90/assets/images/icon-minus.svg";
+        imageElements.src = "https://raw.githubusercontent.com/lmcandido/FAQ/0ff113a00d8f341ef2144542f39c7e33b9aa6f90/assets/images/icon-minus.svg";
     }
 }
