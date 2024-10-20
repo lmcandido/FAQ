29c63c394e91f5cf3c980138e683c8c7ad11102d
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 14:38:08 -0700

    changes

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const imageElements = document.querySelectorAll(".clickable-image");
+function toggleAnswer(element) {
+    element.classList.toggle("active");
+    const answer = element.NextElementSibling;
+    const imageElements = document.querySelector(".clickable-image");
 
-//Define image sources
-const originalImage = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
-const newImageSource = "https://raw.githubusercontent.com/lmcandido/FAQ/0ff113a00d8f341ef2144542f39c7e33b9aa6f90/assets/images/icon-minus.svg";
-
-imageElements.forEach((image) => {
-    const hiddenText = image.nextElementSibling;
-
-    image.addEventListener("click", function(){
-        if (image.src === originalImage){
-            image.src = newImageSource;
-            hiddenText.style.display = "block";
-        } else {
-            image.src = originalImage;
-            hiddenText.style.display = "none";
-        }
-    });
-});
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+        imageElements.src = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
+    } else {
+        answer.style.display = "block";
+        imageElements.src = "https://raw.githubusercontent.com/lmcandido/FAQ/0ff113a00d8f341ef2144542f39c7e33b9aa6f90/assets/images/icon-minus.svg";
+    }
+}
