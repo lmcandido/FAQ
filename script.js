9fe55db1d5678719117a4aca610f772f9a5e15cc
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 14:20:22 -0700

    script changes

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const imageElements = document.querySelectorAll(".clickable-image");
 
+//Define image sources
 const originalImage = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
 const newImageSource = "https://raw.githubusercontent.com/lmcandido/FAQ/0ff113a00d8f341ef2144542f39c7e33b9aa6f90/assets/images/icon-minus.svg";
 
 imageElements.forEach((image) => {
+    const hiddenText = image.nextElementSibling;
 
-    image.addEventListener("click", function() {
-
+    image.addEventListener("click", function(){
         if (image.src === originalImage){
             image.src = newImageSource;
+            hiddenText.computedStyleMap.display = "block";
         } else {
             image.src = originalImage;
+            hiddenText.computedStyleMap.display = "none";
         }
     });
 });
