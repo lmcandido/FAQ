7a866bab697e6cff84187967a3a46a609d8b49fd
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 10:35:30 -0700

    updates

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function toggleSwitch(element){
+function toggleAnswer(element) {
     element.classList.toggle("active");
 
     const answer = elementNextElementSibling;
-    if(answer.style.display === "block"){
+    if (answer.style.display === "block") {
         answer.style.display = "none";
         element.querySelector('.plus-sign').textContent = "+";
-    } else{
+    } else {
         answer.style.display = "block";
         element.querySelector('.plus-sign').textContent = "-";
     }
