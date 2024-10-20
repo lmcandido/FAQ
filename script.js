1adedfbed93babffe78747ce5386879a53b66e74
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 12:37:21 -0700

    changes

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let faqquestion = document.querySelectorAll('.faq-question');
+function toggleAnswer(element) {
+    element.classList.toggle("active");
+    const answer = element.nextElementSibling;
 
-faqquestion.forEach(question => {
-    question.addEventListener('click', function() {
-        const answer = element.nextElementSibling;
-        answer.classList.toggle("active");
-    });
-});
+    if (answer.style.display === "block") {
+        answer.style.display = "none";
+        element.querySelector('.plus-sign').textContent = "+";
+    } else {
+        answer.style.display = "block";
+        element.querySelector('.plus-sign').textContent = "-";
+    }
+}
