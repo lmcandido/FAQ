a82aa8e8a7dccfc30fa99d05298587b4bbf60ab5
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 13:29:38 -0700

    script change

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function toggleAnswer(element) {
-    element.classList.toggle("active");
-    const answer = element.nextElementSibling;
-    const image = element.querySelector('.clickable-image')
+const imageElements = document.querySelectorAll(".clickable-image");
 
-    const image1 = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
-    const image2 = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-minus.svg";
+const originalImage = "https://raw.githubusercontent.com/lmcandido/FAQ/262c5f514deea2f347245ccff6da1d1553bbf944/assets/images/icon-plus.svg";
+const newImageSource = "https://raw.githubusercontent.com/lmcandido/FAQ/0ff113a00d8f341ef2144542f39c7e33b9aa6f90/assets/images/icon-minus.svg";
 
-    if (answer.style.display === "block") {
-        answer.style.display = "none";
-        image.scr = image1;
-    } else {
-        answer.style.display = "block";
-        image.scr = image2;
-    }
-}
+imageElements.forEach((image) => {
+    const answer = image.nextElementSibling;
+
+    image.addEventListener("click", function(){
+
+        if (image.src === originalImage){
+            image.src = newImageSource;
+            answer.computedStyleMap.display = "block";
+        } else {
+            image.src = originalImage;
+            answer.computedStyleMap.display = "none";
+        }
+    });
+});
