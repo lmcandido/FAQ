ca18b883c27bd7ee0dccadb53e17b7bc93706c66
Lucas Candido <candidolucas1993@gmail.com>
2024-10-20 20:23:54 -0700

    changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const answer = document.querySelector('.faq-answer');
+const image = document.querySelector('.faq-icon');
 
-console.log(answer);
+image.addEventListener('click', function() {
+        // Toggle the visibility of the hidden text
+        if (answer.style.display === "none") {
+            answer.style.display = "block"; // Show the text
+        } else {
+            answer.style.display = "none"; // Hide the text
+        }
+});
